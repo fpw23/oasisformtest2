96b7194249be969b0ba04e51dd1833847f4c171f
Frank Wisniewski <me@frankwisniewski.net>
2020-03-23 09:49:28 -0500

    Updates

---
+++ precache-manifest.73154ac52c15d1f99455da89f5a258b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e493147fcaaa287d1b4feb56e18fe2",
+    "url": "/oasisformtest2/index.html"
+  },
+  {
+    "revision": "bc79d8b90df545cec9c7",
+    "url": "/oasisformtest2/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "85cdeb467cbe713d94f9",
+    "url": "/oasisformtest2/static/css/main.a04bb18e.chunk.css"
+  },
+  {
+    "revision": "bc79d8b90df545cec9c7",
+    "url": "/oasisformtest2/static/js/2.981b9795.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/oasisformtest2/static/js/2.981b9795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85cdeb467cbe713d94f9",
+    "url": "/oasisformtest2/static/js/main.51c4db06.chunk.js"
+  },
+  {
+    "revision": "693212afb66ace532f46",
+    "url": "/oasisformtest2/static/js/runtime-main.f91754dc.js"
+  }
+]);
